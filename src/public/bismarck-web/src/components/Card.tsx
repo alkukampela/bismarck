d9449d6989794b2a93cdbdf76f17d50b5a82a796
ri⚡to <risto.kuustera@gmail.com>
2020-04-18 17:36:39 +0300

    Fully colored cards in Girassol font

--- src/public/bismarck-web/src/components/Card.tsx
+++ src/public/bismarck-web/src/components/Card.tsx
@@ -5,9 +5,18 @@ export default class Card extends React.Component<any, {}> {
     super(props);
   }
 
+  private getCardClass = (suit: string) => {
+    switch (suit) {
+      case '♦️':
+      case '♥️':
+        return 'card red-card';
+    }
+    return 'card black-card';
+  };
+
   render() {
     return (
-      <div className="card">
+      <div className={this.getCardClass(this.props.card.suit)}>
         <div className="suit">{this.props.card.suit}</div>
         <div className="rank">{this.props.card.rank}</div>
       </div>
