8dd60bd1b69c3f723124c6c9a2f6a22c57c6a1a7
ri⚡to <risto.kuustera@gmail.com>
2020-04-21 18:58:42 +0300

    Use fetch cards using async functions (bye bye IE11 support)

--- src/public/bismarck-web/src/components/PlayersHand.tsx
+++ src/public/bismarck-web/src/components/PlayersHand.tsx
@@ -1,22 +1,29 @@
 import * as React from 'react';
 import { Card } from './Card';
+import { Card as CardType } from '../../../../types/card';
+
+interface Cards {
+  cards: CardType[];
+}
 
 export const PlayersHand = ({ player }: { player: String }) => {
-  const [state, setState] = React.useState({ cards: [] });
+  const [state, setState] = React.useState<Cards>({ cards: [] });
 
   React.useEffect(() => {
-    fetch(`http://localhost:3001/api/cards?player=${player}`, {
-      mode: 'cors',
-    })
-      .then(response => {
-        return response.json();
-      })
-      .then(cards => {
-        setState(state => ({ ...state, cards }));
-      })
-      .catch(error => {
-        console.log(error);
-      });
+    const fetchCards = async function(): Promise<CardType[]> {
+      const resp = await fetch(
+        `http://localhost:3001/api/cards?player=${player}`,
+        {
+          mode: 'cors',
+        }
+      );
+      const result = await resp.json();
+      return result as CardType[];
+    };
+
+    fetchCards().then(cards => {
+      setState(state => ({ ...state, cards }));
+    });
   }, []);
   return (
     <div className="playersCards">
