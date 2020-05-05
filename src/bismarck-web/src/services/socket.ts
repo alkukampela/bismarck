118bb5a486ecc19c5a4a347ba569f4191d73890c
ri⚡to <risto.kuustera@gmail.com>
2020-05-05 22:54:00 +0300

    Use correct websocket uri in different environments

--- src/bismarck-web/src/services/socket.ts
+++ src/bismarck-web/src/services/socket.ts
@@ -1,6 +1,28 @@
+import { parse } from 'url';
+
 const initSocket = () => {
-  const WS_URL = process.env.REACT_APP_WS_URL || 'UNDEFINED';
-  return new WebSocket(`${WS_URL}?gameId=451`);
+  const hups = (host: string, protocol?: string) => {
+    const wsProtocol = protocol === 'http:' ? 'ws:' : 'wss';
+    return `${wsProtocol}//${host}`;
+  };
+
+  const wsUrlFromHttpUrl = (url: string): string => {
+    const parsed = parse(url);
+    console.log(parsed);
+    return hups(parsed.host || '', parsed.protocol);
+  };
+
+  const wsUrlFromLocation = (url: Location): string => {
+    return hups(url.host, url.protocol);
+  };
+
+  const API_URL = process.env.REACT_APP_API_URL;
+
+  const wsUrl = !!API_URL
+    ? wsUrlFromHttpUrl(API_URL)
+    : wsUrlFromLocation(window.location);
+
+  return new WebSocket(`${wsUrl}?gameId=451`);
 };
 
 export const socket = initSocket();
