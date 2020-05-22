5312a6128a7eea951b642c4d2ad569e9d75b65f0
ri⚡to <risto.kuustera@gmail.com>
2020-05-22 20:38:46 +0300

    Replace tslint with eslint

---
+++ src/bismarck-web/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', '@typescript-eslint/tslint'],
+  rules: {
+    'react/jsx-uses-vars': 1,
+    'no-unused-vars': 1,
+  },
+};
