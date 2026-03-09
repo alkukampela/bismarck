72185b0ef1783fa1fbc4f0f8e45a5d7a5431eb4e
ri⚡to <risto.kuustera@gmail.com>
2026-03-09 22:39:19 +0200

    Fix tables in instructions page

--- src/bismarck-web/vite.config.ts
+++ src/bismarck-web/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import mdx from '@mdx-js/rollup';
+import remarkGfm from 'remark-gfm';
 
 export default defineConfig({
-  plugins: [react(), mdx()],
+  plugins: [react(), mdx({ remarkPlugins: [remarkGfm] })],
   build: {
     outDir: 'dist',
   },
