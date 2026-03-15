2a7509caa2f834327cd9f6fa2758b83065ffe81f
ri⚡to <risto.kuustera@gmail.com>
2026-03-15 21:16:43 +0200

    Improve modal showing hand scores

--- src/bismarck-web/src/components/HandScores.tsx
+++ src/bismarck-web/src/components/HandScores.tsx
@@ -33,27 +33,34 @@ export const HandScores = ({
   const showButton = (): boolean => !!game.player || !!game.token;
 
   return (
-    <div
-      style={{
-        display: isVisible() ? 'block' : 'none',
-      }}
-      className="hand-scores-container"
-    >
-      <h1>Jaon tulokset</h1>
-      {handScores(scores.trickScores).map(
-        (playerScore: PlayerScore, index: number) => (
-          <div key={index}>
-            {playerScore.player.name}: {playerScore.score}
-          </div>
-        )
-      )}
-      <button
-        type="button"
-        onClick={moveToNextHand}
-        style={{ display: showButton() ? 'block' : 'none', marginTop: '1rem' }}
+    <div>
+      <div
+        className="hand-scores-overlay"
+        style={{ display: isVisible() ? 'block' : 'none' }}
+      ></div>
+      <div
+        className="hand-scores-container"
+        style={{ display: isVisible() ? 'block' : 'none' }}
       >
-        Seuraava jako
-      </button>
+        <h1>Jaon tulokset</h1>
+        {handScores(scores.trickScores).map(
+          (playerScore: PlayerScore, index: number) => (
+            <div key={index}>
+              {playerScore.player.name}: {playerScore.score}
+            </div>
+          )
+        )}
+        <button
+          type="button"
+          onClick={moveToNextHand}
+          style={{
+            display: showButton() ? 'block' : 'none',
+            marginTop: '1rem',
+          }}
+        >
+          Seuraava jako
+        </button>
+      </div>
     </div>
   );
 };
